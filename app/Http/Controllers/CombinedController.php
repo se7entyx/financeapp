92d2ee1c00582032344bad50b991642ed2e7729d
yulllllll <ceoofyulll@gmail.com>
2024-07-30 07:58:35 +0700

    frek

--- app/Http/Controllers/CombinedController.php
+++ app/Http/Controllers/CombinedController.php
@@ -13,8 +13,8 @@ class CombinedController extends Controller
     //
     public function getTandaTerima()
     {
-        $tandaTerimaRecords = TandaTerima::with(['supplier', 'user'])->get();
-        $buktiKasRecords = BuktiKas::with(['tanda_terima', 'user'])->get();
+        $tandaTerimaRecords = TandaTerima::with(['supplier', 'user'])->orderBy('created_at', 'desc')->get();
+        $buktiKasRecords = BuktiKas::with(['tanda_terima', 'user'])->orderBy('created_at', 'desc')->get();
         $suppliers = Supplier::orderBy('name', 'asc')->get();
 
         $title = 'All Document';
@@ -22,8 +22,8 @@ class CombinedController extends Controller
     }
     public function getBuktiKas()
     {
-        $tandaTerimaRecords = TandaTerima::with(['supplier', 'user'])->get();
-        $buktiKasRecords = BuktiKas::with(['tanda_terima', 'user'])->get();
+        $tandaTerimaRecords = TandaTerima::with(['supplier', 'user'])->orderBy('created_at', 'desc')->get();
+        $buktiKasRecords = BuktiKas::with(['tanda_terima', 'user'])->orderBy('created_at', 'desc')->get();
         $suppliers = Supplier::orderBy('name', 'asc')->get();
 
         $title = 'All Document';
@@ -33,8 +33,8 @@ class CombinedController extends Controller
     public function getMyTandaTerima()
     {
         $id = auth()->id();
-        $tandaTerimaRecords = TandaTerima::with(['supplier', 'user'])->where('user_id', $id)->get();
-        $buktiKasRecords = BuktiKas::with(['tanda_terima', 'user'])->where('user_id', $id)->get();
+        $tandaTerimaRecords = TandaTerima::with(['supplier', 'user'])->where('user_id', $id)->orderBy('created_at', 'desc')->get();
+        $buktiKasRecords = BuktiKas::with(['tanda_terima', 'user'])->where('user_id', $id)->orderBy('created_at', 'desc')->get();
         $suppliers = Supplier::orderBy('name', 'asc')->get();
         $title = 'My Documents';
         return view('mydoc', ['tandaTerimaRecords' => $tandaTerimaRecords, 'title' => $title, 'buktiKasRecords' => $buktiKasRecords, 'suppliers' => $suppliers]);
@@ -43,8 +43,8 @@ class CombinedController extends Controller
     public function getMyBuktiKas()
     {
         $id = auth()->id();
-        $tandaTerimaRecords = TandaTerima::with(['supplier', 'user'])->where('user_id', $id)->get();
-        $buktiKasRecords = BuktiKas::with(['tanda_terima', 'user'])->where('user_id', $id)->get();
+        $tandaTerimaRecords = TandaTerima::with(['supplier', 'user'])->where('user_id', $id)->orderBy('created_at', 'desc')->get();
+        $buktiKasRecords = BuktiKas::with(['tanda_terima', 'user'])->where('user_id', $id)->orderBy('created_at', 'desc')->get();
         $suppliers = Supplier::orderBy('name', 'asc')->get();
 
         $title = 'My Documents';
